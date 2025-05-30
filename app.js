e453f04732ff9b35169cac0e02c6cb4e58c7390e
ITASanRamon <52384094+ITASanRamon@users.noreply.github.com>
2025-05-29 21:50:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const serverless = require('serverless-http');
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
