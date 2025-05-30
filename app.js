ab7630524b82e50a30f65c90dd0cc85081b9db60
ITASanRamon <52384094+ITASanRamon@users.noreply.github.com>
2025-05-29 20:36:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
