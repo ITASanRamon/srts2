4529bb53fb06ea20a19693dacda3a6d1953840fa
ITASanRamon <52384094+ITASanRamon@users.noreply.github.com>
2025-05-29 20:55:44 -0700

    Update index.ejs from index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.ejs'));
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
